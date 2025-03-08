a8eb700853d8c31af4ce35a66c0f5329e6b3c48a
Florent Martinier <florent.martinier@free.fr>
2025-03-08 14:58:18 +0100

    Deploy frontend

---
+++ local_deploy.ps1
@@ -0,0 +1,18 @@
+$paths = ".\node_modules\", ".\frontend\killer-party-front\node_modules\", ".\frontend\killer-party-front\dist\", ".\src\main\resources\static"
+foreach ($filePath in $paths)
+{
+    if (Test-Path $filePath)
+    {
+        Remove-Item -r -fo $filePath
+        Write-Host "Path $filePath removed"
+    }
+    else
+    {
+        Write-Host "Path $filePath doesn't exits"
+    }
+}
+npm install ./frontend/killer-party-front
+npm run build --prefix ./frontend/killer-party-front
+./gradlew clean
+./gradlew build
+java -jar build/libs/killerpartyback-0.0.1-SNAPSHOT.jar
